ca9b78b2fddab7b1cd5f011fb71df5e4b4b72d69
Aaron <869788668@qq.com>
2018-12-10 22:13:50 +0800

    change the scale of yolo

--- run_this.sh
+++ run_this.sh
@@ -4,6 +4,10 @@
 #./darknet partial cfg/yolov3-tiny.cfg weights/yolov3-tiny.weights yolov3-tiny.conv.15 15
 
 
-# 训练网络
-./darknet detector train cfg_wyz/turtlebot/turtlebot.data cfg_wyz/turtlebot/yolov3-tiny.cfg pretrain_network/yolov3-tiny.conv.15
-
+# 训练网络origin
+./darknet detector train cfg_wyz/turtlebot/turtlebot.data cfg_wyz/turtlebot/yolov3-tiny.cfg pretrain_network/yolov3-tiny.conv.15 -map \
+ | tee output.txt
+# 使用训练后的网络
+# 
+# ./darknet detector train cfg_wyz/turtlebot/turtlebot.data cfg_wyz/turtlebot/yolov3-tiny.cfg pretrain_network/yolov3-tiny.conv.15 -map \
+# | tee output.txt
